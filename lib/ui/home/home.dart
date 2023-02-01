9c44205dacae96131a37f70a3e1e99a1bf3701a6
김민재 <kmj94102@naver.com>
2023-02-01 19:10:01 +0900

    홈 화면 버튼 추가

--- lib/ui/home/home.dart
+++ lib/ui/home/home.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:pokedex_f/ui/list/list.dart';
+import 'package:pokedex_f/utils/constants.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -11,23 +12,45 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   @override
-  void initState() {
-    super.initState();
-
-    Future.delayed(const Duration(milliseconds: 500), (){
-      Navigator.push(
-          context,
-          MaterialPageRoute(
-              builder: (BuildContext context) => const ListScreen()));
-    });
-  }
-
-  @override
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
         body: Column(
-          children: const [Text('홈 화면')],
+          children: [
+            Card(
+              margin: const EdgeInsets.only(left: 24, right: 24, top: 30),
+              elevation: 6,
+              child: InkWell(
+                onTap: () {
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (BuildContext context) =>
+                              const ListScreen()));
+                },
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Container(
+                      padding: const EdgeInsets.only(left: 13),
+                      child: const Text(
+                        '전국 도감',
+                        style: TextStyle(
+                            fontFamily: fontMaruBuri,
+                            fontSize: 24,
+                            fontWeight: FontWeight.bold,
+                            color: Color(0xFFED6035)),
+                      ),
+                    ),
+                    const Spacer(),
+                    Align(
+                        alignment: Alignment.bottomRight,
+                        child: Image.asset('${imagesAddress}img_dex.png'))
+                  ],
+                ),
+              ),
+            )
+          ],
         ),
       ),
     );
